131e843720490b39298fe35caa7ccc868346c8b8
Tushar Mathur <tusharmath@gmail.com>
2016-09-05 17:22:53 +0530

    refactor(store): use a custom implementation of redux
    custom implementation can be easily optimized for performance and smaller library footprint

---
+++ src/micro-redux.js
@@ -0,0 +1,36 @@
+/**
+ * Created by tushar.mathur on 05/09/16.
+ */
+
+'use strict'
+
+const INIT = {type: '@@redux/INIT'}
+
+export class Store {
+  constructor (reducer, state) {
+    this.__reducer = reducer
+    this.__state = state
+    this.__listners = []
+    this.__dispatching = false
+    this.dispatch(INIT)
+  }
+
+  subscribe (listener) {
+    const i = this.__listners.push(listener)
+    return () => this.__listners.splice(i - 1, 1)
+  }
+
+  getState () {
+    return this.__state
+  }
+
+  dispatch (action) {
+    if (this.__dispatching) return
+    this.__dispatching = true
+    this.__state = this.__reducer(this.__state, action)
+    this.__listners.forEach(i => i(this.__state))
+    this.__dispatching = false
+  }
+}
+
+export const createStore = (reducer, state) => new Store(reducer, state)
