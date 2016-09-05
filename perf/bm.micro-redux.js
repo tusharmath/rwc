85e7a8ac79a4ac7340dce7858324be6232a9b63b
Tushar Mathur <tusharmath@gmail.com>
2016-09-05 18:32:38 +0530

    chore(benchmarks): add benchmarkjs integration

---
+++ perf/bm.micro-redux.js
@@ -0,0 +1,16 @@
+/**
+ * Created by tushar.mathur on 05/09/16.
+ */
+
+'use strict'
+
+import {createStore} from '../src/micro-redux'
+import {Suite} from 'benchmark'
+
+const suite = new Suite()
+const store = createStore(x => x, {a: 1})
+store.subscribe(x => x)
+suite
+  .add('dispatch()', () => store.dispatch({type: 'LAME'}))
+  .on('cycle', event => console.log(String(event.target)))
+  .run({'async': true})
